6d2be6fe28c07429538ad54e151a48d16b5c520b
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-20 00:07:27 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,26 @@
-def my_each # put argument(s) here
-  # code here
-end
+  it "test that the block is run n times" do
+    tas = ['arel', 'jon', 'logan', 'spencer']
+    expected = tas.length
+    times_called = 0
+
+    my_each(tas) do |ta|
+      times_called += 1
+    end
+
+    expect(times_called).to eq(expected)
+
+  end
+
+  it "that only a single element is passed into that block." do
+
+    tas = ['arel', 'jon', 'logan', 'spencer']
+    expected = tas.length
+    times_called = 0
+
+    my_each(tas) do |ta|
+      # cannot be an array
+      expect(ta.kind_of?(Array)).to eq(false)
+      expect(ta.kind_of?(String)).to eq(true)
+    end
+
+  end
